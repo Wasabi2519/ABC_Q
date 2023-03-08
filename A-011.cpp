c2c2753b9c335c078b0228852dd743ad936c25c2
たしなむわさび | Wasabi2519 <fuga.maeji@gmail.com>
2023-03-08 14:29:15 +0900

    delete A-011-2

--- A-011.cpp
+++ A-011.cpp
@@ -1,15 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void nextMonth(int& a, int threshold, int subtraction) {
-    if(a > threshold - 11) {
-        a -= subtraction;
-    }
-}
-
 int main(){
     int a;
     cin >> a;
-    nextMonth(a, 12, 11)if{a, 11, +1}
+    a+=1;
+
+    if (a==13){
+        a = 1;
+    }
     cout << a << endl;
 }
